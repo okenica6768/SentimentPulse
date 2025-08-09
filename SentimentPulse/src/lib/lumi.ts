4ca540fd9b1f052fca92102018173bfc8e028a09
okenica6768 <aronokenica@gmail.com>
2025-08-09 18:06:12 +0200

    Delete SentimentPulse directory

--- SentimentPulse/src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p343459386981576704',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
-
